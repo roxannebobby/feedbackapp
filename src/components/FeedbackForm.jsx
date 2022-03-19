ee67ab2472ba16ac7ec72c8620571d7d05250f66
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-03-19 15:37:18 -0500

    text entry validation and isDisabled function

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,20 @@ import Button from './shared/Button'
 
 const FeedbackForm = () => {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
+    if(text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if(text !== '' && text.trim().length <= 10 ) {
+      setBtnDisabled(true)
+      setMessage('Text must be at least 10 characters in length')
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
     setText(e.target.value)
   }
 
@@ -16,8 +28,9 @@ const FeedbackForm = () => {
         {/* todo - rating select component */}
         <div className='input-group'>
           <input onChange={handleTextChange} type='text' placeholder='Write a review' value={text} />
-          <Button type="submit" version='secondary' >Send</Button>
+          <Button type="submit" isDisabled={btnDisabled} version='secondary' >Submit</Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
